45f257301872db8e98312384af5a6da864f4b7b0
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-18 18:37:15 -0300

    a

--- src/MayfiClient.js
+++ src/MayfiClient.js
@@ -1,28 +1,26 @@
 const { Client } = require("discord.js")
 const { readdir } = require("fs")
-const path = require('path');
-const dirPath = path.join(__dirname, '/listeners');
+const Loaders = require("./loaders")
 
 module.exports = class MayfiClient extends Client {
 	constructor(CLIENT_OPTIONS = {}) {
 		super(CLIENT_OPTIONS)
 
-		this.initializeListeners()
+		this.initializeLoaders()
 	}
 
 	login (token = process.env.DISCORD_TOKEN) {
     	return super.login(token);
   }
 
-  initializeListeners(path = dirPath) {
-    console.log(__dirname)
-    readdir(path, (err, files) => {
-      if(err) return console.log(err)
-
-        files.forEach(evt => {
-          const event = new (require(path)(this))
-          super.on(evt.split(".")[0], (...args) => event.run(...args))
-        })
-    })
+  initializeLoaders () {
+    for (let name in Loaders) {
+      try {
+        let Loader = new Loaders[name](this)
+        Loader.load()
+      } catch (err) {
+        console.error(err)
+      }
+    }
   }
 }

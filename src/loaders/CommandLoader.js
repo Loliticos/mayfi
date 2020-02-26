fddf44ab35d3ca9de06720272c2b1872ce0ca9f1
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-25 22:45:03 -0300

    a

--- src/loaders/CommandLoader.js
+++ src/loaders/CommandLoader.js
@@ -2,7 +2,7 @@ const { Collection } = require('discord.js');
 const { readdirSync } = require('fs');
 const chalk = require('chalk')
 
-module.exports = class CommandLoader  {
+module.exports = class EventLoader  {
     constructor(client) {
         this.client = client
     }
@@ -10,8 +10,8 @@ module.exports = class CommandLoader  {
     load () {
         try {
             console.log(chalk.green("Commands are initializing"))
-            this.client.commands = new Collection()
-            this.client.alias = new Collection()
+            this.client.commands = new Collection();
+            this.client.alias = new Collection();
             this.initializeCommands()
             return true
         } catch (err) {
@@ -24,7 +24,7 @@ module.exports = class CommandLoader  {
         for (let category of commandsCategory) {
             let commandFile = readdirSync(`src/commands/${category}`)
             for (let name of commandFile) {
-                const command = new(require(`src/commands/${category}/${name}`))(this.client)
+                const command = new(require('../commands/'+category+'/'+name.split('.js')[0]))(this.client)
                 this.client.commands.set(command.name, command)
                 command.alias.forEach(a => this.client.alias.set(a, command.name))
             }

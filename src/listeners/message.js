f6b0f439913c165cc135c4f75ac67ac098181772
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-25 22:30:32 -0300

    a

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -1,4 +1,4 @@
-const EventHandler = require('../structures/EventHandler');
+const { EventHandler, CommandContext } = require('../');
 const i18next = require('i18next')
 
 module.exports = class ClientOnMessage extends EventHandler {

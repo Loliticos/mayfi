fc0679a9073c95146681d85ab817443d1de39f91
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-26 16:09:22 -0300

    a

--- src/structures/command/index.js
+++ src/structures/command/index.js
@@ -2,5 +2,6 @@ module.exports = {
   Command: require('./Command.js'),
   CommandContext: require('./CommandContext.js'),
   CommandError: require('./CommandError.js'),
+  CommandParameters: require('./parameters/CommandParameters.js'),
   CommandRequirements: require('./CommandRequirements.js')
 }

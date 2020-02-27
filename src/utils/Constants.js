b0cad98f9ec452710e6d958207b0866a2bb0436b
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-26 21:56:42 -0300

    a

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,10 @@
 module.exports = {
   ERROR_COLOR: '#FF3333',
   GUILD_LOST_COLOR: '#BD4351',
-  GUILD_ADDED_COLOR: '#41A57A'
+  GUILD_ADDED_COLOR: '#41A57A',
+  dnd: "<:dnd:573893804109856788>",
+  idle: "<:idle:573893860024123422>",
+  online: "<:online:677264684855853101>",
+  offline: "<:offline:677264835854991421>",
+  streaming: "<:streaming:677331504728244254>"
 }

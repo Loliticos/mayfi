fdb13793f7d7f15534291a009dc45a80e7894794
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-26 21:49:55 -0300

    a

--- src/structures/command/parameters/types/UserParameter.js
+++ src/structures/command/parameters/types/UserParameter.js
@@ -1,18 +1,43 @@
-const UserParameter = require('./UserParameter.js')
+const Parameter = require('./Parameter.js')
+const CommandError = require('../../CommandError.js')
+const PermissionUtils = require('../../../../utils/PermissionUtils.js')
 
-module.exports = class MemberParameter extends UserParameter {
+const MENTION_REGEX = /^(?:<@!?)?([0-9]{16,18})(?:>)?$/
+const defVal = (o, k, d) => typeof o[k] === 'undefined' ? d : o[k]
+
+module.exports = class UserParameter extends Parameter {
   static parseOptions (options = {}) {
     return {
       ...super.parseOptions(options),
-      fetchUser: false
+      acceptBot: !!options.acceptBot,
+      acceptUser: defVal(options, 'acceptUser', true),
+      acceptDeveloper: defVal(options, 'acceptDeveloper', true),
+      acceptSelf: !!options.acceptSelf,
+      errors: {
+        invalidUser: 'errors:invalidUser',
+        acceptSelf: 'errors:sameUser',
+        acceptBot: 'errors:invalidUserBot',
+        acceptUser: 'errors:invalidUserNotBot',
+        acceptDeveloper: 'errors:userCantBeDeveloper',
+        ...(options.errors || {})
+      }
     }
   }
 
-  static parse (arg, context) {
+  static parse (arg, { t, client, author, guild }) {
     if (!arg) return
 
-    const { guild } = context
-    const user = super.parse(arg, context)
-    return guild.member(user)
+    const regexResult = MENTION_REGEX.exec(arg)
+    const id = regexResult && regexResult[1]
+    const findMember = guild.members.find(m => m.user.username.toLowerCase().includes(arg.toLowerCase()) || m.displayName.toLowerCase().includes(arg.toLowerCase()))
+
+    const user = client.users.get(id) || (!!findMember && findMember.user)
+    if (!user) throw new CommandError(t(this.errors.invalidUser))
+    if (!this.acceptSelf && user.id === author.id) throw new CommandError(t(this.errors.acceptSelf))
+    if (!this.acceptBot && user.bot) throw new CommandError(t(this.errors.acceptBot))
+    if (!this.acceptUser && !user.bot) throw new CommandError(t(this.errors.acceptUser))
+    if (!this.acceptDeveloper && PermissionUtils.isDeveloper(client, user)) throw new CommandError(t(this.errors.acceptDeveloper), false)
+
+    return user
   }
 }

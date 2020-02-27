53914eefa5d74ee620b8177150471fba0b17b55e
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-27 19:07:50 -0300

    Make thing better

--- src/commands/social/rep.js
+++ src/commands/social/rep.js
@@ -30,13 +30,13 @@ module.exports = class Rep extends Command {
       return channel.send(embed)
     }
 
-    const dataUser = await this.client.database.users.findOne({_id: user.id})
+    const { reps: UserReps } = await this.client.database.users.findOne({_id: user.id})
 
     await Promise.all([
       this.client.database.users.updateOne({_id: author.id}, { lastRep: Date.now() }),
-      this.client.database.users.updateOne({_id: user.id}, { reps: dataUser.reps += 1})
+      this.client.database.users.updateOne({_id: user.id}, { reps: UserReps += 1})
     ])
-    
+
     channel.send(embed.setDescription(t('commands:rep.repSuccess', { user })))
   }
 }

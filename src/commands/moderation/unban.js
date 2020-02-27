7b7a95ec878647ad5debca9251d2f682b0acc28f
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-27 11:10:49 -0300

    Add locales

--- src/commands/moderation/unban.js
+++ src/commands/moderation/unban.js
@@ -9,11 +9,13 @@ module.exports = class Unban extends Command {
       requirements: { guildOnly: true, botPermissions: ['BAN_MEMBERS'], permissions: ['BAN_MEMBERS'] },
       parameters: [{
         type: 'user', acceptBot: true, missingError: 'commands:unban.missingUser'
+      }, {
+        type: 'string', full: true, missingError: 'commands:unban.missingReason'
       }]
     }, client)
   }
 
-  async run ({ channel, guild, author, t }, user) {
+  async run ({ channel, guild, author, t }, user, reason) {
     const embed = new MayfiEmbed(author)
     await guild.unban(user).then(async unbannedMember => {
         embed

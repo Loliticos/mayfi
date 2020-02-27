3b4ca9fffd021d264a92cc5f6a0a87e0c69ba368
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-26 23:47:23 -0300

    a

--- src/commands/utility/serverinfo.js
+++ src/commands/utility/serverinfo.js
@@ -27,7 +27,7 @@ module.exports = class Serverinfo extends Command {
       .addField(t("commands:guildinfo.joinedAt"), `${moment(guild.joinedTimestamp).format('LLL')}\n(${moment(guild.joinedTimestamp).fromNow()})`, true)
       .addField(t("commands:guildinfo.createdAt"), `${moment(guild.createdAt).format('LLL')}\n(${moment(guild.createdAt).fromNow()})`, true)   
       .addField(t('commands:guildinfo.members', { count: guild.members.size }), [
-        `${Constants.streaming} ${t('commands:guildinfo.streaming', { count: guild.members.filter(m => m.game === 'streaming'.size })}`,
+        `${Constants.streaming} ${t('commands:guildinfo.streaming', { count: guild.members.filter(m => m.game === 'streaming').size })}`,
         `${Constants.online} Online: ${guild.members.filter(m => m.presence.status === 'online'.size}`,
         `${Constants.idle} ${t('commands:guildinfo.idle', { count: guild.members.filter(m => m.presence.status === 'idle').size, language })}`,
         `${Constants.dnd} ${t('commands:guildinfo.dnd', { count: guild.members.filter(m => m.presence.status === 'dnd').size })}`,

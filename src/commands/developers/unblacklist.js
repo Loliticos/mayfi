08a5102464c0825c2fae69f027c9cd5e835bb9f4
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-27 18:00:52 -0300

    a

---
+++ src/commands/developers/unblacklist.js
@@ -0,0 +1,28 @@
+const { Command, MayfiEmbed, Constants } = require('../../')
+
+module.exports = class Unblacklist extends Command {
+  constructor (client) {
+    super({
+      name: 'unblacklist',
+      category: 'developers',
+      hidden: true,
+      requirements: { devOnly: true },
+      parameters: [{
+        type: 'user', showUsage: false, missingError: 'commands:unblacklist.missingUser'
+      }]
+    }, client)
+  }
+
+  async run ({ channel, author, t }, user) {
+    const embed = new MayfiEmbed(author)
+    const userDocument = this.client.database && await this.client.database.users.findOne({_id: user.id})
+    if(!userDocument.blacklisted) {
+      embed.setDescription(t('commands:unblacklist.notBlacklisted'))
+      .setColor(Constants.ERROR_COLOR)
+      return channel.send(embed)  
+    }
+    await this.client.database.users.updateOne({_id: user.id}, { blacklisted: false })
+    embed.setDescription(`**${t('commands:unblacklist.success', { user })}**`)
+    channel.send(embed)
+  }
+}

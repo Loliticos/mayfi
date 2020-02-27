2b8a5854aca3c25569aeb5e7033176c624ff3b60
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-27 16:43:05 -0300

    a

--- database.js
+++ database.js
@@ -1,5 +1,8 @@
 const { connect, Schema, model} = require('mongoose')
 
+const transformProps = require('transform-props')
+const castToString = arg => String(arg)
+
 connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (err) return console.log('[DB] => Ocorreu um erro na conexão.')
   console.log('[DB] Connection estabilished with success!')
@@ -20,6 +23,14 @@ const GuildSchema = new Schema({
 	language: { type: String, default: "pt-BR" },
 	logsChannel: { type: String, default: "false" }
 })
+
+parse (entity) {
+   return entity ? transformProps(entity.toObject({ versionKey: false }), castToString, '_id') : null
+}
+
+UserSchema.methods.findOne = (id, projection) => {
+  return this.model.findById(id, projection).then(parse)
+}
 
 module.exports.users = model('users', UserSchema)
 module.exports.guilds = model('guilds', GuildSchema)

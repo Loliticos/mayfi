16285efffa7d075842784ca685529e23aef3b70e
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-28 20:12:19 -0300

    a

--- src/commands/social/leaderboard.js
+++ src/commands/social/leaderboard.js
@@ -25,14 +25,25 @@ module.exports = class Leaderboard extends Command {
       return channel.send(embed)
     }
 
-    if(text.toLowerCase() !== "reps" && text.toLowerCase() !== "level") {
+    const lowerCased = text.toLowerCase()
+
+    if(lowerCased !== "reps" && lowerCased !== "level") {
       embed
         .setTitle(t("commands:leaderboard.invalidType"))
         .setDescription(t("commands:leaderboard.description", { prefix }))
       return channel.send(embed)
     }
 
-    const dbRes = await this.client.database.users.find({}, text.toLowerCase()).sort({ [text.toLowerCase()]: -1 }).limit(5 + 6)
+    const dbRes = await this.client.database.users.find({}, lowerCased).sort({ [lowerCased]: -1 }).limit(5 + 6)
+    const points;
+    if(text.toLowerCase() == "level") {
+      const dbResPoints = await this.client.database.users.find({}, "exp").sort({ ["exp"]: -1 }).limit(5 + 6)
+      const topToCheckPoints = dbRes.filter(u => {
+        u.user = this.client.users.get(u._id)
+        return !!u.user
+      })
+      const pointsTop = topToCheckPoints.splice(0, 5)
+    }
     const topToCheck = dbRes.filter(u => {
       u.user = this.client.users.get(u._id)
       return !!u.user
@@ -44,9 +55,9 @@ module.exports = class Leaderboard extends Command {
     embed
       .setTitle(t(`commands:leaderboard.title${text}`))
       .setDescription(`
-        **1.** __${this.client.users.get(top[0]._id)} (${this.client.users.get(top[0]._id).tag})__\n**${t(`commands:leaderboard.${text == "reps" ? "reps" : "level"}`)}**: ${top[0][text == "reps" ? "reps" : "level"]} ${text == "level" ? `- **${t("commands:leaderboard.points")}**: ` : ""}\n
-        **2.** __${this.client.users.get(top[1]._id)} (${this.client.users.get(top[1]._id).tag})__\n**${t(`commands:leaderboard.${text == "reps" ? "reps" : "level"}`)}**: ${top[1][text == "reps" ? "reps" : "level"]} ${text == "level" ? `- **${t("commands:leaderboard.points")}**:` : ""}\n
-        **3.** __${this.client.users.get(top[2]._id)} (${this.client.users.get(top[2]._id).tag})__\n**${t(`commands:leaderboard.${text == "reps" ? "reps" : "level"}`)}**: ${top[2][text == "reps" ? "reps" : "level"]} ${text == "level" ? `- **${t("commands:leaderboard.points")}**:` : ""}\n
+        **1.** __${this.client.users.get(top[0]._id)} (${this.client.users.get(top[0]._id).tag})__\n**${t(`commands:leaderboard.${lowerCased == "reps" ? "reps" : "level"}`)}**: ${top[0][lowerCased == "reps" ? "reps" : "level"]} ${text == "level" ? `- **${t("commands:leaderboard.points")}**: ${pointsTop[0].exp}` : ""}\n
+        **2.** __${this.client.users.get(top[1]._id)} (${this.client.users.get(top[1]._id).tag})__\n**${t(`commands:leaderboard.${lowerCased == "reps" ? "reps" : "level"}`)}**: ${top[1][lowerCased == "reps" ? "reps" : "level"]} ${text == "level" ? `- **${t("commands:leaderboard.points")}**: ${pointsTop[0].exp}` : ""}\n
+        **3.** __${this.client.users.get(top[2]._id)} (${this.client.users.get(top[2]._id).tag})__\n**${t(`commands:leaderboard.${lowerCased == "reps" ? "reps" : "level"}`)}**: ${top[2][lowerCased == "reps" ? "reps" : "level"]} ${text == "level" ? `- **${t("commands:leaderboard.points")}**: ${pointsTop[0].exp}` : ""}\n
         `)
     channel.send(embed)
 

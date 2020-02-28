1f855c096bd4d238fa8634e4cfa248bb2b63086a
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-28 19:40:07 -0300

    a

--- src/commands/social/leaderboard.js
+++ src/commands/social/leaderboard.js
@@ -32,7 +32,7 @@ module.exports = class Leaderboard extends Command {
       return channel.send(embed)
     }
 
-    const dbRes = await this.client.database.users.find({}, "reps").sort({ ["reps"]: -1 }).limit(5 + 6)
+    const dbRes = await this.client.database.users.find({}, text.toLowerCase()).sort({ [text.toLowerCase()]: -1 }).limit(5 + 6)
     const topToCheck = dbRes.filter(u => {
       u.user = this.client.users.get(u._id)
       return !!u.user
@@ -42,9 +42,9 @@ module.exports = class Leaderboard extends Command {
     embed
       .setTitle(t(`commands:leaderboard.title${text}`))
       .setDescription(`
-        **1.** __${this.client.users.get(top[0]._id)} (${this.client.users.get(top[0]._id).tag})__\n**${t("commands:leaderboard.reps")}**: ${top[0].reps}\n
-        **2.** __${this.client.users.get(top[1]._id)} (${this.client.users.get(top[1]._id).tag})__\n**${t("commands:leaderboard.reps")}**: ${top[1].reps}\n
-        **3.** __${this.client.users.get(top[2]._id)} (${this.client.users.get(top[2]._id).tag})__\n**${t("commands:leaderboard.reps")}**: ${top[2].reps}\n
+        **1.** __${this.client.users.get(top[0]._id)} (${this.client.users.get(top[0]._id).tag})__\n**${t(`commands:leaderboard.${text == "reps" ? "reps" : "levels"}`)}**: ${top[0].reps}\n
+        **2.** __${this.client.users.get(top[1]._id)} (${this.client.users.get(top[1]._id).tag})__\n**${t(`commands:leaderboard.${text == "reps" ? "reps" : "levels"}`)}**: ${top[1].reps}\n
+        **3.** __${this.client.users.get(top[2]._id)} (${this.client.users.get(top[2]._id).tag})__\n**${t(`commands:leaderboard.${text == "reps" ? "reps" : "levels"}`)}**: ${top[2].reps}\n
         `)
     channel.send(embed)
 

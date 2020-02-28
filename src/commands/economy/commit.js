daf00ba04cf773ea65a18ee3281fb0b9f78bc9cd
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-27 23:33:58 -0300

    a

---
+++ src/commands/economy/commit.js
@@ -0,0 +1,43 @@
+const { Command, MayfiEmbed, CommandError } = require('../../')
+
+module.exports = class Commit extends Command {
+  constructor (client) {
+    super({
+      name: 'commit',
+      aliases: ['change', 'trocar'],
+      category: 'economy',
+      requirements: { databaseOnly: true, },
+      parameters: [{
+        type: 'string', 
+        required: true,
+        missingError: "commands:commit.noArguments"
+      }]
+    }, client)
+  }
+
+  async run ({ channel, guild, author, t }, gems) {
+    const embed = new MayfiEmbed(author)
+
+    try {
+      let UserData = await this.client.database.users.findOne({_id: author.id})
+
+      if(UserData.gems < gems) {
+        embed
+          .setTitle(t("commands:commit.noValue"))
+          .setDescription(t("commands:commit.youCantCommit", { gems }))
+        return channel.send(embed)
+      }
+
+      await this.client.database.users.updateOne({_id: author.id}, { gems: UserData.gems -= gems, money: UserData.money += gems * 4 })
+
+      const transfered = gems * 4
+      embed
+        .setTitle(t("commands:commit.title"))
+        .setDescription(t("commands:commit.transfered", { gems, transfered }))
+ 
+      channel.send(embed)
+    } catch(err) {
+      throw new CommandError(t("errors:generic"))
+    }
+  }
+}

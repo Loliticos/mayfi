80ef42c45bc0ca1764ec8b57797d9fe21697dc41
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-28 19:43:07 -0300

    a

--- src/commands/social/leaderboard.js
+++ src/commands/social/leaderboard.js
@@ -25,6 +25,8 @@ module.exports = class Leaderboard extends Command {
       return channel.send(embed)
     }
 
+    console.log(text)
+
     if(text.toLowerCase() !== "reps" || text.toLowerCase() !== "levels") {
       embed
         .setTitle(t("commands:leaderboard.invalidType"))

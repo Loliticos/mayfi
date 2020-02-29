14d135e98e1795b7dce77194d9a8a1bd9d65c37a
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-02-29 14:59:01 -0300

    Add new database function

--- src/MayfiClient.js
+++ src/MayfiClient.js
@@ -6,8 +6,12 @@ module.exports = class MayfiClient extends Client {
 	constructor(CLIENT_OPTIONS = {}) {
 		super(CLIENT_OPTIONS)
 
-		this.initializeLoaders()
-    this.database = require("../database.js")
+    this.databaseLoaded = null
+
+    this.initializeLoaders()
+    this.initializeDatabase()
+
+    this.database = this.databaseLoaded
 	}
 
 	login (token = process.env.DISCORD_TOKEN) {
@@ -17,6 +21,15 @@ module.exports = class MayfiClient extends Client {
   async runCommand (command, context, args, language) {
     context.setFixedT(this.i18next.getFixedT(language))
     return command._run(context, args).catch(console.error)
+  }
+
+  async initializeDatabase () {
+    try {
+      this.databaseLoaded = require("../database.js")
+    } catch (e) {
+      this.databaseLoaded = null
+      console.error(e)
+    }
   }
 
   async initializeLoaders () {

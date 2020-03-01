8e00dda6b55ce1320c55baa834b0d949488cdc63
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-01 18:25:06 -0300

    Add command docs

---
+++ src/commands/bot/docs.js
@@ -0,0 +1,17 @@
+const { Command, MayfiEmbed } = require('../../')
+
+module.exports = class Docs extends Command {
+  constructor (client) {
+    super({
+      name: 'docs',
+      category: 'bot'
+    }, client)
+  }
+
+  async run ({ t, channel }) {
+    const embed = new MayfiEmbed()
+    embed.setThumbnail(this.client.user.displayAvatarURL)
+      .setDescription(`[${t('commands:docs.clickHere')}](https://mayfi.gitbook.io/docs/)`)
+    channel.send(embed)
+  }
+}

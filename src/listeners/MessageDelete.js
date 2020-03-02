015db1da8c6486a7d6b4a1abb96fcb72c0b40add
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-02 17:15:38 -0300

    Fix logs system

--- src/listeners/MessageDelete.js
+++ src/listeners/MessageDelete.js
@@ -6,21 +6,25 @@ module.exports = class MessageDelete extends EventHandler {
     }
 
     async run(message) {
-    	if(message.author.bot) return
+        try {
+            if(message.author.bot) return
 
-    	const author = message.author
+            const author = message.author
 
-        const dataGuild = await this.client.database.guilds.findOne({_id: message.guild.id})
+            const dataGuild = await this.client.database.guilds.findOne({_id: message.guild.id})
 
-        if(!dataGuild || !dataGuild.logsChannel || !dataGuild.language) return
+            if(!dataGuild || !dataGuild.logsChannel || !dataGuild.language) return
 
-    	const t = this.client.i18next.getFixedT(dataGuild.language)
+            const t = this.client.i18next.getFixedT(dataGuild.language)
 
-    	const embed = new MayfiEmbed(author)
-    	.setTitle(t("commands:logs.deletedMessage"))
-    	.addField(t("commands:logs.author"), `${author} - ${author.tag}`)
-    	.addField(t("commands:logs.channel"), message.channel)
-    	.addField(t("commands:logs.message"), message.content)
-  		message.guild.channels.get(dataGuild.logsChannel).send({embed: embed})
+            const embed = new MayfiEmbed(author)
+            .setTitle(t("commands:logs.deletedMessage"))
+            .addField(t("commands:logs.author"), `${author} - ${author.tag}`)
+            .addField(t("commands:logs.channel"), message.channel)
+            .addField(t("commands:logs.message"), message.content)
+            message.guild.channels.get(dataGuild.logsChannel).send({embed: embed})
+        } catch(err) {
+            console.error(err)
+        }
     }
 }
--- src/listeners/MessageUpdate.js
+++ src/listeners/MessageUpdate.js
@@ -6,27 +6,31 @@ module.exports = class MessageUpdate extends EventHandler {
     }
 
     async run(oldMessage, newMessage) {
-        if(newMessage.author.bot) return
-        if(!newMessage || !oldMessage) return
+        try {
+            if(newMessage.author.bot) return
+            if(!newMessage || !oldMessage) return
 
-        if(oldMessage.content === newMessage.content) return
-        this.client.emit("message", newMessage)
+            if(oldMessage.content === newMessage.content) return
+            this.client.emit("message", newMessage)
 
-        const author = newMessage.author
+            const author = newMessage.author
 
-        const dataGuild = await this.client.database.guilds.findOne({_id: newMessage.guild.id})
+            const dataGuild = await this.client.database.guilds.findOne({_id: newMessage.guild.id})
 
-        if(!dataGuild || !dataGuild.logsChannel || !dataGuild.language) return
+            if(!dataGuild || !dataGuild.logsChannel || !dataGuild.language) return
 
-        const t = this.client.i18next.getFixedT(dataGuild.language)
+            const t = this.client.i18next.getFixedT(dataGuild.language)
 
-        const embed = new MayfiEmbed(author)
-        .setTitle(t("commands:logs.editedMessage"))
-        .setDescription(`**[${t("commands:logs.goToMessage")}](${newMessage.url})**`)
-        .addField(t("commands:logs.author"), `${author} - ${author.tag}`)
-        .addField(t("commands:logs.channel"), newMessage.channel)
-        .addField(t("commands:logs.oldMessage"), oldMessage.content)
-        .addField(t("commands:logs.newMessage"), newMessage.content)
-        newMessage.guild.channels.get(dataGuild.logsChannel).send(embed)
+            const embed = new MayfiEmbed(author)
+            .setTitle(t("commands:logs.editedMessage"))
+            .setDescription(`**[${t("commands:logs.goToMessage")}](${newMessage.url})**`)
+            .addField(t("commands:logs.author"), `${author} - ${author.tag}`)
+            .addField(t("commands:logs.channel"), newMessage.channel)
+            .addField(t("commands:logs.oldMessage"), oldMessage.content)
+            .addField(t("commands:logs.newMessage"), newMessage.content)
+            newMessage.guild.channels.get(dataGuild.logsChannel).send(embed)
+        } catch (e) {
+            console.error(e)
+        }
     }
 }

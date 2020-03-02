a091ec639d13d71cf9e7e45556c3d3768044e0ec
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-02 13:25:56 -0300

    Add command addrole

---
+++ src/commands/moderation/addrole.js
@@ -0,0 +1,37 @@
+const { Command, MayfiEmbed, CommandError } = require('../../')
+
+module.exports = class Addrole extends Command {
+  constructor (client) {
+    super({
+      name: 'addrole',
+      aliases: ['adicionarcargo'],
+      category: 'moderation',
+      requirements: { guildOnly: true, botPermissions: ['MANAGE_ROLES'], permissions: ['MANAGE_ROLES'] },
+      parameters: [{
+        type: 'member',
+        required: true,
+        acceptSelf: true,
+        acceptBot: true
+      }, {
+        type: 'role', required: true, full: false
+      }]
+    }, client)
+  }
+
+  async run ({ channel, guild, author, t }, member, role) {
+    const embed = new MayfiEmbed(author)
+
+    try {
+      member.addRole(role.id)
+      .then(() => {
+        embed
+          .setTitle(t("commands:addrole.title"))
+          .setDescription(t("commands:addrole.description"), { member, role })
+        channel.send({ embed: embed })
+      })
+    } catch(e) {
+      throw new CommandError(`${t("errors:generic")}\n\`${e.message}\``)
+    }
+
+  }
+}

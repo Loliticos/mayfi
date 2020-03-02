1b2772d3645192e513c96e9466cbae396146b2ae
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-02 18:33:00 -0300

    Fix userinfo

--- src/commands/utility/userinfo.js
+++ src/commands/utility/userinfo.js
@@ -8,27 +8,28 @@ module.exports = class Userinfo extends Command {
       aliases: ['ui'],
       category: 'utility',
       parameters: [{
-        type: 'member', full: true, required: false, acceptBot: true
+        type: 'user', full: true, required: false, acceptBot: true
       }]
     }, client)
   }
 
-  async run ({ channel, member: author, t, language }, member = author) {
+  async run ({ channel, member: author, t, language, guild }, user = author) {
 
       moment.locale(language)
 
-      const UserData = await this.client.database.users.findOne({_id: member.id})
+      const UserData = await this.client.database.users.findOne({_id: user.id})
+      const member = guild.members.get(user.id)
 
-      let embed = new MayfiEmbed(member.user)
-      .setAuthor(member.displayName, member.user.displayAvatarURL)
-      .addField("ID", member.user.id, true)
-      .addField("Tag", member.user.tag, true)
+      let embed = new MayfiEmbed(user)
+      .setAuthor(user.displayName, user.displayAvatarURL)
+      .addField("ID", user.id, true)
+      .addField("Tag", user.tag, true)
       .addField("Status", t(`commands:userinfo.${member.presence.status}`, { status: Constants[member.presence.status] }))
-      .addField(t("commands:userinfo.playing"), member.user.presence.game ? member.user.presence.game : t("commands:userinfo.notPlaying"))
-      .addField(t("commands:userinfo.createdAt"), `${moment(member.user.createdTimestamp).format('LLL')}\n(${moment(member.user.createdTimestamp).fromNow()})`)
+      .addField(t("commands:userinfo.playing"), user.presence.game ? user.presence.game : t("commands:userinfo.notPlaying"))
+      .addField(t("commands:userinfo.createdAt"), `${moment(user.createdTimestamp).format('LLL')}\n(${moment(user.createdTimestamp).fromNow()})`)
       .addField(t("commands:userinfo.joinedAt"), `${moment(member.joinedTimestamp).format('LLL')}\n(${moment(member.joinedTimestamp).fromNow()})`)     
       UserData ? embed.addField(t("commands:userinfo.aboutMe"), UserData.personalText) : " "
-      embed.setThumbnail(member.user.displayAvatarURL)
+      embed.setThumbnail(user.displayAvatarURL)
 
       channel.send(embed)
         

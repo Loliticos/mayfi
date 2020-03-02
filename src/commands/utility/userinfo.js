564cbc166dddb4ab9199a6d12ac524188f71ea0a
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-02 18:42:08 -0300

    a

--- src/commands/utility/userinfo.js
+++ src/commands/utility/userinfo.js
@@ -8,28 +8,32 @@ module.exports = class Userinfo extends Command {
       aliases: ['ui'],
       category: 'utility',
       parameters: [{
-        type: 'user', full: true, required: false, acceptBot: true
+        type: 'member', full: true, required: false, acceptBot: true
       }]
     }, client)
   }
 
-  async run ({ channel, member: author, t, language, guild }, user = author) {
+  async run ({ channel, member: author, t, language, guild }, member = author) {
 
       moment.locale(language)
 
-      const UserData = await this.client.database.users.findOne({_id: user.id})
+      const filter = this.client.guilds.filter(g => g.members.has(member.id)).map(g => g.name)
+      const charLimit = (s) => s.length > 1024 ? `${s.substr(0, 1020)}...` : s
+
+      const UserData = await this.client.database.users.findOne({_id: member.user.id})
       const member = guild.members.get(user.id)
 
-      let embed = new MayfiEmbed(user)
-      .setAuthor(user.displayName, user.displayAvatarURL)
-      .addField("ID", user.id, true)
-      .addField("Tag", user.tag, true)
-      .addField("Status", t(`commands:userinfo.${user.presence.status}`, { status: Constants[user.presence.status] }))
-      .addField(t("commands:userinfo.playing"), user.presence.game ? user.presence.game : t("commands:userinfo.notPlaying"))
-      .addField(t("commands:userinfo.createdAt"), `${moment(user.createdTimestamp).format('LLL')}\n(${moment(user.createdTimestamp).fromNow()})`)
+      let embed = new MayfiEmbed(member.user)
+      .setAuthor(member.user.displayName, member.user.displayAvatarURL)
+      .addField("ID", member.user.id, true)
+      .addField("Tag", member.user.tag, true)
+      .addField("Status", t(`commands:userinfo.${member.user.presence.status}`, { status: Constants[member.user.presence.status] }))
+      .addField(t("commands:userinfo.playing"), member.user.presence.game ? member.user.presence.game : t("commands:userinfo.notPlaying"))
+      .addField(t("commands:userinfo.createdAt"), `${moment(member.user.createdTimestamp).format('LLL')}\n(${moment(member.user.createdTimestamp).fromNow()})`)
       .addField(t("commands:userinfo.joinedAt"), `${moment(member.joinedTimestamp).format('LLL')}\n(${moment(member.joinedTimestamp).fromNow()})`)     
       UserData ? embed.addField(t("commands:userinfo.aboutMe"), UserData.personalText) : " "
-      embed.setThumbnail(user.displayAvatarURL)
+      .addField(t('commands:userinfo.serversInCommon', { count: filter.length }), charLimit(filter.join(', ')))
+      embed.setThumbnail(member.user.displayAvatarURL)
 
       channel.send(embed)
         

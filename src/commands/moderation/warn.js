276c3e697e88ee565864efcaec4162d04ab38e09
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-03 17:32:20 -0300

    add warns systems

---
+++ src/commands/moderation/warn.js
@@ -0,0 +1,23 @@
+const { Command, MayfiEmbed } = require('../../')
+
+module.exports = class Warns extends Command {
+  constructor (client) {
+    super({
+      name: 'warns',
+      aliases: ['avisos'],
+      category: 'moderation',
+      parameters: [{
+        type: 'member', full: true, required: false, acceptBot: true
+      }]
+    }, client)
+  }
+
+  async run ({ channel, member: author, t }, member = author) {
+    const { warns } = await this.database.users.findOne({_id: member.user.id})
+
+    const embed = new MayfiEmbed(author)
+    .setTitle(t("commands:warns.userWarns", { warns, member }))
+    channel.send(embed)
+
+  }
+}
---
+++ src/commands/moderation/warns.js
@@ -0,0 +1,37 @@
+const { Command, MayfiEmbed, Constants } = require('../../')
+
+module.exports = class Warn extends Command {
+  constructor (client) {
+    super({
+      name: 'warn',
+      aliases: ['avisar'],
+      category: 'moderation',
+      requirements: { guildOnly: true, botPermissions: ['KICK_MEMBERS'], permissions: ['KICK_MEMBERS'] },
+      parameters: [{
+        type: 'member', required: true, acceptSelf: false, missingError: "commands:warn.noMember"
+      }, {
+        type: 'string', required: false, full: true
+      }]
+    }, client)
+  }
+
+  async run ({ channel, author, t }, member, string) {
+    const embed = new MayfiEmbed(author)
+
+    try {
+      await this.client.database.users.updateOne({_id: member.user.id}, { $inc: { warns: 1 } })
+
+      embed
+        .setTitle(t("commands:warn.warned"))
+        .setDescription(`${member} ${string ? "-" string : ""}`)
+    } catch (e) {
+      embed
+        .setColor(Constants.ERROR_COLOR)
+        .setTitle(t("commands:warn.cantWarn"))
+        .setDescription(`\`${e.message}\``)
+    }
+
+    channel.send(embed)
+
+  }
+}

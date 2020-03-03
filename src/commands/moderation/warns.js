d41cb2734e83c052e8e0969c3c26db742524d65d
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-03 17:39:07 -0300

    Fix another typo

--- src/commands/moderation/warns.js
+++ src/commands/moderation/warns.js
@@ -13,10 +13,10 @@ module.exports = class Warns extends Command {
   }
 
   async run ({ channel, member: author, t }, member = author) {
-    const userData = await this.database.users.findOne({_id: member.user.id})
+    const { warns } = await this.client.database.users.findOne({_id: member.user.id})
 
     const embed = new MayfiEmbed(author)
-    .setTitle(t("commands:warns.userWarns", { warns: userData.warns, member }))
+    .setTitle(t("commands:warns.userWarns", { warns, member }))
     channel.send(embed)
 
   }

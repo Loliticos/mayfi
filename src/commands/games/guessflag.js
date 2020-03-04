b8083c802d5d6bb33057a7f9c546b8e9d4970f8f
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-04 19:18:15 -0300

    a

---
+++ src/commands/games/guessflag.js
@@ -0,0 +1,44 @@
+const { Command, MayfiEmbed, Constants } = require('../../')
+const fetch = require("node-fetch")
+
+module.exports = class Guessflag extends Command {
+  constructor (client) {
+    super({
+      name: 'guessflag',
+      aliases: ["guessflags", 'guesstheflag'],
+      category: 'games'
+    }, client)
+  }
+
+  async run ({ channel, author, t}) {
+    let embed = new MayfiEmbed(author)
+    const choosenCountry = await fetch("https://api.printful.com/countries").then(res => res.json())
+
+    const country = choosenCountry.result[Math.floor(Math.random() * choosenCountry.result.length)]
+    console.log(country)
+    embed
+      .setTitle(t("commands:guessflag.guessTheFlag"))
+      .setDescription(t("commands:guessflag.country"))
+      .setThumbnail(`https://www.countryflags.io/${country.code.toLowerCase()}/flat/64.png`)
+    channel.send(embed)
+
+    const filter = c => c.author.equals(author) && c.content.toLowerCase() == country.name.toLowerCase() || c.content.toLowerCase() == country.code.toLowerCase()
+
+    channel.awaitMessages(filter, { time: 10000, max: 1 })
+      .then(collected => {
+        embed = new MayfiEmbed(author)
+        if (collected.size > 0) {
+          embed
+            .setTitle(t("commands:guessflag.youGotIt"))
+            .setDescription(t("commands:guessflag.theWordWas", { country }))
+          return channel.send({ embed })
+        } else {
+          embed
+            .setColor(Constants.ERROR_COLOR)
+            .setTitle(t("commands:guessflag.timeout"))
+          return channel.send({ embed })
+        }
+      })
+    
+  }
+}

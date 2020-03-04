a699974b0f2ef6d8b0a22ebaca48094c363ab9a8
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-04 20:08:50 -0300

    a

--- src/commands/games/guessflag.js
+++ src/commands/games/guessflag.js
@@ -1,6 +1,6 @@
 const { Command, MayfiEmbed, Constants } = require('../../')
 const fetch = require("node-fetch")
-const { Attachment } = require("discord.js")
+const emojiList = ['🔄']
 
 module.exports = class Guessflag extends Command {
   constructor (client) {
@@ -15,30 +15,61 @@ module.exports = class Guessflag extends Command {
     let embed = new MayfiEmbed(author)
     const choosenCountry = await fetch("https://api.printful.com/countries").then(res => res.json())
 
-    const country = choosenCountry.result[Math.floor(Math.random() * choosenCountry.result.length)]
-    embed
-      .setTitle(t("commands:guessflag.guessTheFlag"))
-      .setDescription(t("commands:guessflag.country"))
-      .setThumbnail(`https://www.countryflags.io/${country.code.toLowerCase()}/flat/64.png`)
-    channel.send(embed)
+    function play() {
+      const country = choosenCountry.result[Math.floor(Math.random() * choosenCountry.result.length)]
 
-    const filter = c => c.author.equals(author) && c.content.toLowerCase() == country.name.toLowerCase() || c.content.toLowerCase() == country.code.toLowerCase()
+      console.log(country)
 
-    channel.awaitMessages(filter, { time: 10000, max: 1 })
-      .then(collected => {
-        embed = new MayfiEmbed(author)
-        if (collected.size > 0) {
-          embed
-            .setTitle(t("commands:guessflag.youGotIt"))
-            .setDescription(t("commands:guessflag.theWordWas", { country }))
-          return channel.send({ embed })
-        } else {
-          embed
-            .setColor(Constants.ERROR_COLOR)
-            .setTitle(t("commands:guessflag.timeout"))
-          return channel.send({ embed })
-        }
-      })
+      embed
+        .setTitle(t("commands:guessflag.guessTheFlag"))
+        .setDescription(t("commands:guessflag.country"))
+        .setThumbnail(`https://www.countryflags.io/${country.code.toLowerCase()}/flat/64.png`)
+      channel.send(embed)
+
+      const filter = c => c.author.equals(author) && c.content.toLowerCase() == country.name.toLowerCase() || c.content.toLowerCase() == country.code.toLowerCase()
+
+      channel.awaitMessages(filter, { time: 10000, max: 1 })
+        .then(collected => {
+          embed = new MayfiEmbed(author)
+          if (collected.size > 0) {
+            embed
+              .setTitle(t("commands:guessflag.youGotIt"))
+              .setDescription(t("commands:guessflag.theWordWas", { country }))
+
+            return channel.send({ embed }).then(async (msg) => {
+              for (const emoji of emojiList) await msg.react(emoji)
+
+              const reactionCollector = await msg.createReactionCollector(
+                (reaction, user) => emojiList.includes(reaction.emoji.name) && !user.bot,
+                { time: 10000 })
+
+              reactionCollector.on('collect', (reaction) => {
+
+                switch (reaction.emoji.name) {
+                  case emojiList[0]:
+                    play()
+                    msg.delete()
+                    break;
+                }
+              })
+
+              reactionCollector.on('end', (result) => {
+                if(!result.size) {
+                  msg.clearReactions()
+                }
+
+              })
+            })
+          } else {
+            embed
+              .setColor(Constants.ERROR_COLOR)
+              .setTitle(t("commands:guessflag.timeout"))
+            return channel.send({ embed })
+          }
+        })
+    }
+
+    play()
     
   }
 }

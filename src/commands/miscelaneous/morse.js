8fd86b42764594a34a2e9a62092515416ef66f27
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-04 13:34:19 -0300

    add unmute command

--- src/commands/miscelaneous/morse.js
+++ src/commands/miscelaneous/morse.js
@@ -18,6 +18,8 @@ module.exports = class Morse extends Command {
 
     if(text.includes(".") || text.includes("-")) {
       const morseToEnglish = await fetch(`http://api.funtranslations.com/translate/morse2english.json?text=${encodeURIComponent(text)}`).then(res => res.json())
+      console.log(morseToEnglish)
+      
       embed
         .setTitle(t("commands:morse.morseToText"))
         .setDescription(morseToEnglish.contents.text) 
---
+++ src/commands/moderation/unmute.js
@@ -0,0 +1,74 @@
+const { Command, MayfiEmbed, Constants } = require('../../')
+const fs = require("fs")
+
+module.exports = class Unute extends Command {
+  constructor (client) {
+    super({
+      name: 'unmute',
+      aliases: ['desmutar'],
+      category: 'moderation',
+      requirements: { guildOnly: true, botPermissions: ['KICK_MEMBERS'], permissions: ['MANAGE_ROLES'] },
+      parameters: [{
+        type: 'member', missingError: 'commands:unmute.missingUser'
+      }]
+    }, client)
+  }
+
+  async run ({ channel, guild, author, t }, member) {
+    const embed = new MayfiEmbed(author)
+    let mutedRole = guild.roles.find(r => r.name == t("commands:unmute.roleName"))
+
+    if(!mutedRole) {
+      mutedRole = await guild.createRole({
+        name: t("commands:unmute.roleName"),
+        color: "#7a7a7a"
+      })
+
+      guild.channels.forEach(async (c) => {
+        await c.overwritePermissions(mutedRole, {
+          SEND_MESSAGES: false,
+          ADD_REACTION: false,
+          SEND_TTS_MESSAGES: false,
+          ATTACH_FILES: false,
+          SPEAK: false
+        })
+      })
+    }
+
+    if(!member.roles.has(mutedRole.id)) {
+      return channel.send(
+        embed
+          .setColor(Constants.ERROR_COLOR)
+          .setTitle(t("commands:unmute.notMuted"))
+      )
+    } 
+
+    try {
+
+      fs.writeFile('../../../mute.json', JSON.stringify(this.client.mutes), err => {
+        if (err) console.error
+
+        member.removeRole(mutedRole.id).then(async user => {
+          channel.send(        
+            embed
+              .setTitle(t("commands:unmute.unmuted"))
+              .setDescription(t("commands:unmute.description", { user, time: ms(time) }))
+          )
+        }).catch(err => {
+          embed
+            .setColor(Constants.ERROR_COLOR)
+            .setTitle(t('commands:unmute.cantUnmute'))
+            .setDescription(`\`${err}\``)
+          return channel.send(embed)
+        })
+      })
+
+    } catch(err) {
+        embed
+          .setColor(Constants.ERROR_COLOR)
+          .setTitle(t('commands:unmute.cantUnmute'))
+          .setDescription(`\`${err}\``)
+        return channel.send(embed)
+    }
+  }
+}

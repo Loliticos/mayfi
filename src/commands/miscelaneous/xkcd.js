1920248e827456d328acb37e5c2995191d35f938
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-06 12:30:57 -0300

    Add new commands

---
+++ src/commands/miscelaneous/xkcd.js
@@ -0,0 +1,49 @@
+const { Command, MayfiEmbed, CommandError } = require('../../')
+const xkcd = require("xkcd-node")
+
+module.exports = class Xkcd extends Command {
+  constructor (client) {
+    super({
+      name: 'xkcd',
+      aliases: ['tirinha'],
+      category: 'miscelaneous',
+      parameters: [{
+        type: 'string', full: true, required: false
+      }]
+    }, client)
+  }
+
+  async run ({ t, author, channel }, text) {
+    const embed = new MayfiEmbed()
+
+    let responseData
+
+    try {
+      if (text && text.toLowerCase() === "latest") {
+          xkcd.latest((error, response) => {
+            if (error) console.error(error)
+
+            generateEmbed(response)
+          })
+      } else {
+         xkcd.random((error, response) => {
+           if (error) console.error(error)
+            
+           generateEmbed(response)
+        })
+      }
+    } catch (e) {
+      if (e.statusCode === 404) {
+        throw new CommandError(t("commands:xkcd.notFound"))
+      }
+    }
+
+    function generateEmbed(response) {
+      embed
+        .setTitle(`${response.num} - ${response.title}`)
+        .setDescription(response.alt)
+        .setImage(response.img)
+      return channel.send({embed})
+    }
+  }
+}
--- src/commands/utility/instagram.js
+++ src/commands/utility/instagram.js
@@ -1,4 +1,4 @@
-const { Command, MayfiEmbed } = require('../../')
+const { Command, MayfiEmbed, Constants } = require('../../')
 const fetch = require("node-fetch")
 
 module.exports = class Instagram extends Command {
@@ -8,7 +8,7 @@ module.exports = class Instagram extends Command {
       category: 'utility',
       parameters: [{
         type: 'string',
-        full: true,
+        full: false,
         missingError: 'commands:instagram.invalidUser'
       }]
     }, client)
@@ -17,18 +17,27 @@ module.exports = class Instagram extends Command {
   async run ({ t, channel, author, guild }, user) {
     const embed = new MayfiEmbed(author)
 
-    const body = await fetch(`https://instagram.com/${user}/?__a=1`).then(res => res.json())
-    const res = body.graphql.user
+    try {
+      const body = await fetch(`https://instagram.com/${user}/?__a=1`).then(res => res.json())
 
-    if(!res.username) {
+      const res = body.graphql.user
+
       embed
-        .setTitle("Invalid user")
-      return channel.send(embed)
+        .setTitle(res.full_name)
+        .setURL(res.external_url_linkshimmed)
+        .setThumbnail(res.profile_pic_url)
+        .setDescription(res.biography.length == 0 ? t("commands:instagram.noBiography") : res.biography)
+        .addField(t("commands:instagram.followers"), res.edge_followed_by.count.toLocaleString())
+        .addField(t("commands:instagram.following"), res.edge_follow.count.toLocaleString() + " " + t("commands:instagram.users"))
+        .addField(t("commands:instagram.media"), res.edge_owner_to_timeline_media.count + " " + t("commands:instagram.photos"))
+        .addField(t("commands:instagram.privatedAcoount"), res.is_private ? t("commons:yes") : t("commons:no"))
+      channel.send(embed)
+    } catch(e) {
+        embed
+          .setColor(Constants.ERROR_COLOR)
+          .setTitle(t("commons:instagram.invalidUser"))
+        return channel.send(embed)
     }
-
-    embed
-      .setDescription(res.biography)
-    channel.send(embed)
 
   }
 }

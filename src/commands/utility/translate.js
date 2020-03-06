b84618e46e1af07ae924275b9d1dba5eec30a519
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-06 15:59:38 -0300

    errors

--- src/commands/utility/translate.js
+++ src/commands/utility/translate.js
@@ -1,4 +1,4 @@
-const { Command, MayfiEmbed } = require('../../')
+const { Command, MayfiEmbed, Constants } = require('../../')
 const fetch = require("node-fetch")
 
 module.exports = class Translate extends Command {
@@ -21,29 +21,36 @@ module.exports = class Translate extends Command {
     }, client)
   }
 
-  async run ({ channel, author, t},  to = "en", text) {
+  async run ({ channel, author, t}, to, text) {
+    const embed = new MayfiEmbed(author)
 
-    const from = await fetch(`http://api.languagelayer.com/detect?acces_key=${process.env.TRANSLATE_API}&query=${new URLSearchParams(params).toString()}`).then(res => res.json())
-    
-    console.log(from)
+    try {
+      const from = await fetch(`http://api.languagelayer.com/detect?acces_key=${process.env.TRANSLATE_API}&query=${new URLSearchParams(text).toString()}`).then(res => res.json())
+      
+      console.log(from)
 
-    const params = {
-      sl: from.results.language_code,
-      tl: to,
-      q: text
+      const params = {
+        sl: from.results.language_code,
+        tl: to,
+        q: text
+      }
+
+      const URLqueryParams = new URLSearchParams(params)
+
+      const res = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&dt=t' + `&${URLqueryParams.toString()}`).then(res => res.json())
+
+      const translated = res[0][0][0]
+      
+      embed
+        .setDescription(translated.length > 2000 ? translated.slice(0, 2000) + '...' : translated)
+
+    } catch(e) {
+        embed
+          .setColor(Constants.ERROR_COLOR)
+          .setTitle(t("commands:translate.invalidLanguage"))
     }
 
-    const URLqueryParams = new URLSearchParams(params)
+    channel.send(embed)
 
-    const res = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&dt=t' + `&${URLqueryParams.toString()}`).then(res => res.json())
-
-    const translated = res[0][0][0]
-    console.log(res)
-    
-    let embed = new MayfiEmbed(author)
-    .setDescription(translated.length > 2000 ? translated.slice(0, 2000) + '...' : translated)
-
-    channel.send({embed})
-        
   }
 }

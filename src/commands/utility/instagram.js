b55824f18f8a6187a7ef1a966a095523b7dbb395
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-06 11:19:08 -0300

    logs

---
+++ src/commands/utility/instagram.js
@@ -0,0 +1,27 @@
+const { Command, MayfiEmbed } = require('../../')
+const fetch = require("node-fetch")
+
+module.exports = class Instagram extends Command {
+  constructor (client) {
+    super({
+      name: 'instagram',
+      category: 'utility',
+      parameters: [{
+        type: 'string',
+        full: false,
+        missingError: 'commands:deleteemoji.invalidEmoji'
+      }]
+    }, client)
+  }
+
+  async run ({ t, channel, author, guild }, user) {
+    const embed = new MayfiEmbed(author)
+
+    try {
+      const body = await fetch(`https://instagram.com/${user}/?__a=1`).then(res => res.json())
+
+      console.log(body)
+    }
+
+  }
+}

17a631a8de60e8ef1d1f9d06ffa7d712e26bc601
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-06 16:58:54 -0300

    a

--- src/commands/utility/translate.js
+++ src/commands/utility/translate.js
@@ -40,8 +40,6 @@ module.exports = class Translate extends Command {
 
       const res = JSON.stringify(result)
 
-      console.log(res)
-
       const params = {
         sl: res.language,
         tl: to,
@@ -53,9 +51,22 @@ module.exports = class Translate extends Command {
       const language = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&dt=t' + `&${URLqueryParams.toString()}`).then(res => res.json())
 
       const translated = language[0][0][0]
-        
+
+      const choosenCountry = await fetch("https://api.printful.com/countries").then(r => r.json())
+
+      console.log(res)
+      console.log(res.language)
+      
+      const countryNameTO = choosenCountry.result.find(o => o.code.toString().toLowerCase() === to.toLowerCase())
+      const countryNameFROM = choosenCountry.result.find(o => o.code.toString().toLowerCase() === res.language)
+
+      console.log(countryNameTO)
+      console.log(countryNameFROM)
+
       embed
-        .setDescription(translated.length > 2000 ? translated.slice(0, 2000) + '...' : translated)
+        .setTitle(t("commands:translate.translated"))
+        .addField(`flag_${to} ${countryNameTO.name}`, text.length > 200 ? text.slice(0, 2000) + '...' : text)
+        .addField(`flag_${res.language} ${countryNameFROM.name}`, translated.length > 2000 ? translated.slice(0, 2000) + '...' : translated)
       return channel.send(embed)
     })
   }

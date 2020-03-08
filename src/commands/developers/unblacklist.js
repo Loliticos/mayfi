34ee187229cc72bc8d8511dbf760b403f1c02aaf
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-08 15:40:00 -0300

    a

--- src/commands/developers/unblacklist.js
+++ src/commands/developers/unblacklist.js
@@ -25,7 +25,7 @@ module.exports = class Unblacklist extends Command {
       switch (e.message) {
         case "USER_NOT_BLACKLISTED":
         embed
-          setDescription(t('commands:unblacklist.notBlacklisted'))
+          .setDescription(t('commands:unblacklist.notBlacklisted'))
           .setColor(Constants.ERROR_COLOR)
       }
     }

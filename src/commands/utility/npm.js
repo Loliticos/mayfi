450510e026dc79ca748a608e5dc46d843fea89cf
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-09 17:18:11 -0300

    fixes

--- src/commands/utility/npm.js
+++ src/commands/utility/npm.js
@@ -17,6 +17,8 @@ module.exports = class Npm extends Command {
             
     const embed = new MayfiEmbed(author)
 
+    moment.locale(language)
+
     const pkg = await npm()
     .text(npmPackage)
     .search()
@@ -24,11 +26,11 @@ module.exports = class Npm extends Command {
     console.log(pkg)
 
     embed
-      .setAuthor("npm", "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/042013/npm_0.png?itok=0Jst3N3-")
+      .setAuthor("npm", "https://cdn.freebiesupply.com/logos/large/2x/npm-2-logo-png-transparent.png")
       .setTitle(pkg[0].name)
       .setURL(pkg[0].links.npm)
-      .setDescription(`${pkg[0].description ? pkg[0].description : ""}\n${pkg[0].keywords && pkg[0].keywords.length > 0 ? pkg[0].keywords.map(p => `\`${p}\``).join(", ") : ""}`)
-
+      .setDescription(`${pkg[0].description ? pkg[0].description : ""}\n\n${pkg[0].keywords && pkg[0].keywords.length > 0 ? pkg[0].keywords.map(p => `\`${p}\``).join(", ") : ""}`)
+      .addField(t("commands:npm.published"), `${moment(pkg[0].date).format('LLL')}\n${moment(pkg[0].date).fromNow()}`)
     channel.send(embed)
 
 

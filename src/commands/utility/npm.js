c92aa752efa5115851c0f3e5eb210e07df690d88
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-09 17:29:01 -0300

    more informations

--- src/commands/utility/npm.js
+++ src/commands/utility/npm.js
@@ -30,7 +30,7 @@ module.exports = class Npm extends Command {
       .setTitle(pkg[0].name)
       .setURL(pkg[0].links.npm)
       .setDescription(`${pkg[0].description ? pkg[0].description : ""}\n\n${pkg[0].keywords && pkg[0].keywords.length > 0 ? pkg[0].keywords.map(p => `\`${p}\``).join(", ") : ""}`)
-      .addField(t("commands:npm.lastPublish"), `**v${pkg[0].version}** ${t("commands:npm.publisher")} **${pkg[0].publisher.username}** ${t("commands:npm.date")} **${moment(pkg[0].date).format('LLL')}**\n(${moment(pkg[0].date).fromNow()})`)
+      .addField(t("commands:npm.lastPublish"), `**v${pkg[0].version}** ${t("commands:npm.publisher")} **${pkg[0].publisher.username}** ${t("commands:npm.date")} **${moment(pkg[0].date).fromNow()}**`)
     channel.send(embed)
 
 

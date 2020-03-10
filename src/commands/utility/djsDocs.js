f8bb219120fdcedb2100f9ffd5ce9488425f8653
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-10 16:55:03 -0300

    Fixes, and add command djsdocs

---
+++ src/commands/utility/djsDocs.js
@@ -0,0 +1,32 @@
+const { Command, Constants } = require('../../')
+const qs = require("querystring")
+const fetch = require("node-fetch")
+
+module.exports = class djsDocs extends Command {
+  constructor (client) {
+    super({
+      name: 'djsdocs',
+      aliases: ['djs-docs'],
+      category: 'utility',
+      parameters: [{
+        type: 'string', full: true, required: true, missingError: "commands:djsdocs.invalidDocument"
+      }]
+    }, client)
+  }
+
+  async run ({ channel, author, t, message, language}, query) {
+        
+    const queryString = qs.stringify({ q: query })
+
+    const embed = await fetch(`https://djsdocs.sorta.moe/v2/embed?${queryString}`).then(res => res.json())
+
+    if (!res) {
+      embed
+        .setColor(Constants.ERROR_COLOR)
+        .setTitle(t("commands:djsdocs.invalidDocument"))
+      return channel.send(embed)
+    }
+
+      channel.send(embed)
+  } 
+}

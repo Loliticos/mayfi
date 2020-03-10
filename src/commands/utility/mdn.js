6e3c3eea6d0a4326106df1d9b64c72418f3f49a2
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-10 14:01:18 -0300

    add mdn command

--- src/commands/utility/mdn.js
+++ src/commands/utility/mdn.js
@@ -21,9 +21,7 @@ module.exports = class MDN extends Command {
 
       const queryString = qs.stringify({ q: query })
 
-      const documentToFind = await fetch(`https://mdn.pleb.xyz/search?${queryString}`).then(res => res.json())
-
-      const res = documentToFind.Translations.find(o => o.Locale === language.replace("en-US", "en").replace("fr-FR", "fr").replace("pt-BR", "pt"))
+      const res = await fetch(`https://mdn.pleb.xyz/search?${queryString}`).then(res => res.json())
 
       if (!res) {
         embed
@@ -40,17 +38,11 @@ module.exports = class MDN extends Command {
           replacement: (text, node) => `[${text}](https://developer.mozilla.org${res.URL})`,
       })
 
-      console.log(res)
-
       embed
         .setAuthor("MDN", "https://developer.mozilla.org/static/img/favicon144.png", 'https://developer.mozilla.org/')
         .setURL(`https://developer.mozilla.org${res.URL}`)
         .setTitle(res.Title)
         .setDescription(turndown.turndown(summary))
       channel.send(embed)
-
-    
-    
-    
   } 
 }

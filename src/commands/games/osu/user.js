23328c043ae1017433d82223c80b6cdfd7f63551
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-11 16:17:34 -0300

    fixes

--- src/commands/games/osu/user.js
+++ src/commands/games/osu/user.js
@@ -1,5 +1,6 @@
 const { Command, Constants, MayfiEmbed } = require('../../../')
 const Osu = require('node-osu')
+const moment = require("moment")
 
 module.exports = class OsuUser extends Command {
   constructor (client) {
@@ -17,7 +18,7 @@ module.exports = class OsuUser extends Command {
     }, client)
   }
 
-  async run ({ author, t, channel, guild }, _user) {
+  async run ({ author, t, channel, guild, language }, _user) {
     const embed = new MayfiEmbed(author)
 
     const osu = new Osu.Api(process.env.OSU_API_KEY, {
@@ -30,6 +31,8 @@ module.exports = class OsuUser extends Command {
 
     const user = userData[0]
 
+    moment.locale(language)
+
     embed
       .setAuthor("!osu", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Osu%21Logo_%282015%29.png/600px-Osu%21Logo_%282015%29.png")
       .setDescriptionFromBlockArray([
@@ -37,9 +40,11 @@ module.exports = class OsuUser extends Command {
           `:flag_${user.country.toLowerCase()}: **[${user.username}](https://osu.ppy.sh/u/${user.user_id})** (${t(`commands:${this.path}.level`, { number: Math.floor(user.level) })})`
         ],
         [
-          `**${user.count_rank_ssh}** ${Constants.OSU_SSH}, **${user.count_rank_ss}** ${Constants.OSU_SS}, **${user.count_rank_sh}** ${Constants.OSU_SH}, **${user.count_rank_s}** ${Constants.OSU_S}, **${user.count_rank_a}** ${Constants.OSU_A}`
+          `**${user.count_rank_ssh}** ${Constants.OSU_SSH}, **${user.count_rank_ss}** ${Constants.OSU_SS}, **${user.count_rank_sh}** ${Constants.OSU_S}, **${user.count_rank_s}** ${Constants.OSU_S}, **${user.count_rank_a}** ${Constants.OSU_A}`
         ]
       ])
+      .addField(t(`commands:${this.path}.joined`), `**${moment(user.join_date).format('LLL')}** (${moment(user.join_date).fromNow()})}`)
+      .addField(t(`commands:${this.path}.timePlayed`), `${moment.duration(user.total_seconds_played * 1000).format('d[d] h[h] m[m] s[s]')}`)
       .setColor(this.parentCommand.OSU_COLOR)
     channel.send(embed)
 

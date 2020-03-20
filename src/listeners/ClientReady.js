46c1ce1342c0233b303b79946c34c267b1ce2ca1
Loliticos <47262825+Loliticos@users.noreply.github.com>
2020-03-20 14:51:28 -0300

    Fixes

--- src/listeners/ClientReady.js
+++ src/listeners/ClientReady.js
@@ -56,7 +56,7 @@ module.exports = class ClientOnReady extends EventHandler {
         fetch(`https://botsfordiscord.com/api/bots/${this.client.user.id}`, {
           method: 'POST',
           headers: { Authorization: process.env.BOTSFORDISCORD_TOKEN },
-          body: { server_count: client.guilds.size }
+          body: { server_count: this.client.guilds.size }
         })
         .then(() => console.log('[BFD] Posted statistics successfully'))
         .catch(() => console.log('[BFD] Failed to post statistics'))
